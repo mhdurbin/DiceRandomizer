4b553c19cb21da5c9f44362a8f404071a70e3b94
MDurbin <mhdurbin@users.noreply.github.com>
2019-01-01 21:16:48 -0700

    Logic updates

--- src/DiceRandomizer/DiceRandomizer.java
+++ src/DiceRandomizer/DiceRandomizer.java
@@ -8,7 +8,6 @@ import java.util.Random;
  * Next task is to improve upon the (Pseudo) Random number generator
  * May want to look at timing/efficiency of the program
  * May want to consider a GUI or some other interface
- * May want to set up multiple dice type rolls
  * Another possible task is to implement this in a situation of inheritance for a DND Character sheet
  */
 
@@ -49,16 +48,30 @@ public class DiceRandomizer {
 	
 	// This will be called to create an array of dice rolls
 	// It will call a random number generator to get the random dice rolls
-	// diceRolls will take in the type of dice being rolled and the number of dice being rolled
+	// diceRolls will take the following variables
+		// the type of dice being rolled
+		// the number of dice being rolled
+		// the number of times the dice set should be rolled
+		// the string determining if the lowest die should be subtracted
 	
-	public static void diceRolls(int diceNum, int diceType) {
+	public static void diceRolls(int diceNum, int diceType, int iter, String dndStats) {
 		int[] randomNumArray = new int[diceNum];
+		int sum = 0;
 		
-		for(int i = 0; i < diceNum; i++) {
-			randomNumArray[i] = randomNumGen(diceType);
+		for (int i = 0; i < iter; i++) {
+			for(int j = 0; j < diceNum; j++) {
+				randomNumArray[j] = randomNumGen(diceType);
+				sum += randomNumArray[j];
+			}
+			Arrays.sort(randomNumArray);
+			if (dndStats.equals("Yes")) {
+				sum = sum - randomNumArray[0];
+			}
+			System.out.print("Roll " + (i+1) + " " + Arrays.toString(randomNumArray));
+			System.out.println(" : Sum of Roll " + (i+1) + " is " + sum);
+			sum = 0;		
 		}
 		
-		System.out.println(Arrays.toString(randomNumArray));
 		//JOptionPane.showMessageDialog(null, Arrays.toString(randomNumArray));
 	}
 	
@@ -69,13 +82,23 @@ public class DiceRandomizer {
 		// Declaration of variables
 		// diceNum will indicate the number of dice that need to be rolled
 		// diceType will indicate the type of dice to be rolled i.e. d4, d6, etc
-		// radix is to get the integer value from the String input
+		// num is to get the integer value from the String input
 		// numberOfDice and typeOfDice are the String inputs to get diceNum and diceType
+		// numberOfRolls are the String inputs to determine how many times the set is being rolled
+		// dndStats is to do determine if a die should be subtracted
 		
 		int diceNum = 0;
 		int diceType = 0;
-		int radix = 0;
-		String radixString = null;
+		int iterations = 0;
+		int num = 0;
+		String numString = null;
+		
+		String dndStats = JOptionPane.showInputDialog(null, "Do you want to roll for DND stats (4d6 - lowest)? (Yes or No)", JOptionPane.QUESTION_MESSAGE);
+		
+		if (dndStats == null) {
+			JOptionPane.showMessageDialog(null, "The program has been cancelled.");
+			System.exit(0);
+		}
 		
 		String numberOfDice = JOptionPane.showInputDialog(null, "How many dice would you like rolled?", JOptionPane.QUESTION_MESSAGE);
 		
@@ -93,25 +116,34 @@ public class DiceRandomizer {
 			System.exit(0);
 		}
 		
+		String numberOfRolls = JOptionPane.showInputDialog(null, "How many times would you like the set of dice to be rolled?", JOptionPane.QUESTION_MESSAGE);
+		
+		if (numberOfRolls == null) {
+			JOptionPane.showMessageDialog(null, "The program has been cancelled.");
+			System.exit(0);
+		}
+		
+		iterations = Integer.parseInt(numberOfRolls);
+		
 		// The if statement is to determine the size of the dice being used
 		
 		if(typeOfDice.length() == 2 && typeOfDice.charAt(0) == 'd') {
-			radixString = typeOfDice.substring(1);
-			radix = Integer.parseInt(radixString);
-			diceType = radix;
+			numString = typeOfDice.substring(1);
+			num = Integer.parseInt(numString);
+			diceType = num;
 		}else if(typeOfDice.length() > 2) {
-			radixString = typeOfDice.substring(1, typeOfDice.length());
-			radix = Integer.parseInt(radixString);
-			diceType = radix;
+			numString = typeOfDice.substring(1, typeOfDice.length());
+			num = Integer.parseInt(numString);
+			diceType = num;
 		}else {
 			JOptionPane.showMessageDialog(null, "Please provide the type of dice in the 'd#' format");
 			typeOfDice = JOptionPane.showInputDialog(null, "What type of dice would you like rolled? (d4 = 4, d6 = 6, etc)", JOptionPane.QUESTION_MESSAGE);
-			radix = Integer.parseInt(typeOfDice.substring(1, typeOfDice.length()));
-			diceType = radix;
+			num = Integer.parseInt(typeOfDice.substring(1, typeOfDice.length()));
+			diceType = num;
 		}
 		
-		System.out.println("Below are your results for rolling " + diceNum + typeOfDice + " dice!");
-		diceRolls(diceNum, diceType);
+		System.out.println("Below are your results for rolling " + diceNum + typeOfDice + " dice " + iterations + " times!");
+		diceRolls(diceNum, diceType, iterations, dndStats);
 		
 		// Used for troubleshooting purposes
 //		System.out.println("Number of Dice String: " + numberOfDice);
